8b9005b0e680d85695945d05a96adfc6237edc32
jfjjrmfjg <zafer34amer@gmail.com>
2026-01-06 21:14:40 +0100

    Initial Commit

---
+++ src/main/java/net/jfjjrmfjg/reachhelmet/ReachHelmet.java
@@ -0,0 +1,16 @@
+package net.jfjjrmfjg.reachhelmet;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReachHelmet implements ModInitializer {
+	public static final String MOD_ID = "reach-helmet";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jfjjrmfjg/reachhelmet/ReachHelmetClient.java
@@ -0,0 +1,11 @@
+package net.jfjjrmfjg.reachhelmet;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReachHelmetClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
